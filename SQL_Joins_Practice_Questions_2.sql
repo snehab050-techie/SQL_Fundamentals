76f6202e286b1f94868acfcfb4663c6b820d312d
snehab050-techie <snehab050@gmail.com>
2026-09-20 23:39:27 +0530

    Query to Find the departments where the total salary of all employees is greater than 120,000 and display the department name and total salary

--- SQL_Joins_Practice_Questions_2.sql
+++ SQL_Joins_Practice_Questions_2.sql
@@ -352,3 +352,5 @@ WHERE e1.department_id = e2.department_id);
 #31. Find the employees who work in the IT or HR department and display their name, department name, and salary.
 
 #32. Find the total salary paid by each department and display the department name and total salary.
+
+#33. Find the departments where the total salary of all employees is greater than 120,000 and display the department name and total salary.
